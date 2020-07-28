6d93f2c611a9cb5ef32c64abf99df87966b8ebab
MaitreyiJ <59170109+MaitreyiJ@users.noreply.github.com>
2020-07-28 06:06:55 -0400

    Css and styling

---
+++ PersonalWebsiteStart.js
@@ -0,0 +1,24 @@
+//alert("We started Javascript!")
+// var imageEE=document.getElementById("EEImage");
+// imageEE.style.width="1100px";
+// imageEE.style.height="50%";
+
+
+//alert(textHover.textContent);
+//var bodyElement=document.getElementsByID("body");
+// textHover.addEventListener("mouseover",function(){
+//     //once we  hover on the text, display profile picture
+//    // alert("Hover effects");
+//    // textHover.style.color="green";
+
+
+
+
+// });
+// textHover.addEventListener("mouseout",function(){
+//     textHover.style.color="white";
+
+
+
+// });
+
